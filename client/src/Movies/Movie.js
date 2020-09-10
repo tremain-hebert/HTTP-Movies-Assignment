068800c5d5004a93ad531d05514354dda692022b
Tremain Hebert <truman@Tremains-Mac-mini.local>
2020-09-10 18:03:46 -0500

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
+import UpdateMovie from './UpdateMovie';
 
 function Movie({ addToSavedList }) {
+  const {push} = useHistory();
   const [movie, setMovie] = useState(null);
   const params = useParams();
 
@@ -25,6 +27,17 @@ function Movie({ addToSavedList }) {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
+  
+  const deleteMovie = (e) => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/movies/${movie.id}`)
+      .then((res) => {
+        setMovie(res.data);
+        push('/movies')
+      })
+      .catch((err) => console.log(err));
+  }
 
   return (
     <div className="save-wrapper">
@@ -32,6 +45,12 @@ function Movie({ addToSavedList }) {
 
       <div className="save-button" onClick={saveMovie}>
         Save
+      </div>
+      <div className="edit-button" onClick={() => push(`/update-movie/${movie.id}`)}>
+        Edit
+      </div>
+      <div className="delete-button" onClick={deleteMovie}>
+        Delete
       </div>
     </div>
   );
